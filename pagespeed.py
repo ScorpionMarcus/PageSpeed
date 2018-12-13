69b90216bdb5b1fe105f38b6f16c83bd44ee1e63
Marcus LeGault <marcus.legault@scorpion.co>
2018-12-13 13:02:47 -0800

    commented my code

--- pagespeed.py
+++ pagespeed.py
@@ -14,14 +14,28 @@ json_data = requests.get(api_url)
 data = json.loads(json_data.text)
 
 def download_image(pic_name, pic_url):
+
+	# Request the image URL
 	r = requests.get(pic_url, allow_redirects=True)
+
+	# Declare a save path
 	save_path = 'C:/Users/marcus.legault/Downloads/Pics'
+
+	# Join save path with file name
 	completeName = os.path.join(save_path, pic_name)
+
 	print('Saving ' + pic_name + ' -----> ' + completeName)
+
+	# Write the file to the completeName path
 	open(completeName, 'wb').write(r.content)
 
+# Loop through the image URLs from the JSON
 for item in data['formattedResults']['ruleResults']['OptimizeImages']['urlBlocks'][0]['urls']:
+
 	pic_url = item['result']['args'][0]['value']
+
+	# Split the image name from the URL
 	pic_name = item['result']['args'][0]['value'].split("/")[-1]
-	
+
+	# Input into function
 	download_image(pic_name, pic_url)
