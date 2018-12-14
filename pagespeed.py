9156b7c1e3ad2628745f7f74e845f6acd5473f90
Marcus LeGault <marcus.legault@scorpion.co>
2018-12-14 10:07:46 -0800

    fixed loop and added input for url

--- pagespeed.py
+++ pagespeed.py
@@ -6,8 +6,8 @@ import os.path
 # https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=http://www.performance-law.com&strategy=mobile&key=AIzaSyDJNu6pW7kifKiVssTdMlyad-hUc3stgOg
 
 api_key = "AIzaSyDJNu6pW7kifKiVssTdMlyad-hUc3stgOg"
-url = 'mdaccidentlaw-redesign.scorpionwebsite.com'
-print("Querying " + url)
+url = input('Enter domain: ')
+print("Querying " + url + '...')
 api_url = 'https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=http://' + url + '&strategy=mobile&key=' + api_key
 
 json_data = requests.get(api_url)
@@ -26,7 +26,6 @@ def download_image(pic_name, pic_url):
 	completeName = os.path.join(save_path, pic_name)
 
 	print('Saving ' + pic_name + ' -----> ' + completeName)
-	print(pic_url)
 
 	# Write the file to the completeName path
 	open(completeName, 'wb').write(r.content)
@@ -41,3 +40,7 @@ for item in data['formattedResults']['ruleResults']['OptimizeImages']['urlBlocks
 
 	# Input into function
 	download_image(pic_name, pic_url)
+
+# Loop through the image URL to print references
+for i in data['formattedResults']['ruleResults']['OptimizeImages']['urlBlocks'][0]['urls']:
+	print(i['result']['args'][0]['value'])
