fc03fc139aabcaf1b9656a58548b95330378f098
Marcus LeGault <marcus.legault@scorpion.co>
2018-12-15 00:54:31 -0800

    integrated tinify for image compression

--- pagespeed.py
+++ pagespeed.py
@@ -1,46 +1,44 @@
 import requests
 import json
-import urllib.request
 import os.path
-
-# https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=http://www.performance-law.com&strategy=mobile&key=AIzaSyDJNu6pW7kifKiVssTdMlyad-hUc3stgOg
+import tinify
 
 api_key = "AIzaSyDJNu6pW7kifKiVssTdMlyad-hUc3stgOg"
-url = input('Enter domain: ')
+tinify.key = 'DJxqxb2DBprtLcm5QlsbpsJt8MNrvLkc'
+save_path = 'C:/Users/marcus.legault/scripts/PageSpeed/pics'
+
+print('Enter domain: ')
+url = input().replace('https', '').replace('http', '').replace('/', '').replace(':', '')
 print("Querying " + url + '...')
 api_url = 'https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=http://' + url + '&strategy=mobile&key=' + api_key
 
-json_data = requests.get(api_url)
+# return JSON
+data = json.loads(requests.get(api_url).text)
 
-data = json.loads(json_data.text)
+try:
+    # loop through the image URLs from the JSON
+    for item in data['formattedResults']['ruleResults']['OptimizeImages']['urlBlocks'][0]['urls']:
+        pic_url = item['result']['args'][0]['value']
+        pic_name = item['result']['args'][0]['value'].split("/")[-1]
+        completeName = os.path.join(save_path, pic_name)
 
+        # tinypng pulls from image URLs, compresses, then saves
+        print('Compressing ' + pic_name + '...')
+        tinify.from_url(pic_url).to_file(completeName)
+        print('Saved to ' + completeName)
+
+    # loop through the image URL to print references
+    for i in data['formattedResults']['ruleResults']['OptimizeImages']['urlBlocks'][0]['urls']:
+        print(i['result']['args'][0]['value'])
+
+except KeyError:
+    print(url + ' is not a valid domain name')
+
+except:
+    print('Something went wrong')
+
+'''
 def download_image(pic_name, pic_url):
-
-	# Request the image URL
 	r = requests.get(pic_url, allow_redirects=True)
-
-	# Declare a save path
-	save_path = 'C:/Users/marcus.legault/scripts/PageSpeed/pics'
-
-	# Join save path with file name
-	completeName = os.path.join(save_path, pic_name)
-
-	print('Saving ' + pic_name + ' -----> ' + completeName)
-
-	# Write the file to the completeName path
 	open(completeName, 'wb').write(r.content)
-
-# Loop through the image URLs from the JSON
-for item in data['formattedResults']['ruleResults']['OptimizeImages']['urlBlocks'][0]['urls']:
-
-	pic_url = item['result']['args'][0]['value']
-
-	# Split the image name from the URL
-	pic_name = item['result']['args'][0]['value'].split("/")[-1]
-
-	# Input into function
-	download_image(pic_name, pic_url)
-
-# Loop through the image URL to print references
-for i in data['formattedResults']['ruleResults']['OptimizeImages']['urlBlocks'][0]['urls']:
-	print(i['result']['args'][0]['value'])
+'''
