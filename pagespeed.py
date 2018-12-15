4af85f73cf5e10a97c6238d7fe0a86f7caac0818
Marcus LeGault <marcus.legault@scorpion.co>
2018-12-15 15:59:39 -0800

    updated pagespeed to v5

--- pagespeed.py
+++ pagespeed.py
@@ -3,23 +3,23 @@ import json
 import os.path
 import tinify
 
-api_key = "AIzaSyDJNu6pW7kifKiVssTdMlyad-hUc3stgOg"
+api_key = 'AIzaSyDJNu6pW7kifKiVssTdMlyad-hUc3stgOg'
 tinify.key = 'DJxqxb2DBprtLcm5QlsbpsJt8MNrvLkc'
 save_path = 'C:/Users/marcus.legault/scripts/PageSpeed/pics'
 
 print('Enter domain: ')
-url = input().replace('https', '').replace('http', '').replace('/', '').replace(':', '')
+url = input().replace('https', '').replace('http', '').replace('/', '').replace(':', '').strip()
 print("Querying " + url + '...')
-api_url = 'https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=http://' + url + '&strategy=mobile&key=' + api_key
+api_url = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://' + url + '&strategy=mobile&key=' + api_key
 
 # return JSON
 data = json.loads(requests.get(api_url).text)
 
 try:
     # loop through the image URLs from the JSON
-    for item in data['formattedResults']['ruleResults']['OptimizeImages']['urlBlocks'][0]['urls']:
-        pic_url = item['result']['args'][0]['value']
-        pic_name = item['result']['args'][0]['value'].split("/")[-1]
+    for item in data['lighthouseResult']['audits']['uses-optimized-images']['details']['items']:
+        pic_url = item['url']
+        pic_name = pic_url.split("/")[-1]
         completeName = os.path.join(save_path, pic_name)
 
         # tinypng pulls from image URLs, compresses, then saves
@@ -28,8 +28,8 @@ try:
         print('Saved to ' + completeName)
 
     # loop through the image URL to print references
-    for i in data['formattedResults']['ruleResults']['OptimizeImages']['urlBlocks'][0]['urls']:
-        print(i['result']['args'][0]['value'])
+    for item in data['lighthouseResult']['audits']['uses-optimized-images']['details']['items']:
+        print(item['url'])
 
 except KeyError:
     print(url + ' is not a valid domain name')
@@ -38,6 +38,7 @@ except:
     print('Something went wrong')
 
 '''
+# This will download the image directly rather than go through tinify
 def download_image(pic_name, pic_url):
 	r = requests.get(pic_url, allow_redirects=True)
 	open(completeName, 'wb').write(r.content)
